5b514102796ff4b6dd92844637ac91781b195aca
linjh <516308181@qq.com>
2023-04-13 15:55:04 +0800

    feat(user): 完成用户管理

--- miniprogram/pages/my/index.js
+++ miniprogram/pages/my/index.js
@@ -4,4 +4,18 @@ Page({
       url: '/pages/login/index',
     })
   },
+  onShow() {
+    // 获取登录用户信息
+    this.getUserInfo()
+  },
+
+  // 用户信息接口
+  async getUserInfo() {
+    // 请求数据接口
+    const { code, data: userInfo } = await wx.http.get('/userInfo')
+    // 校验数据是否合法
+    if (code !== 10000) return wx.utils.toast('数据加载失败, 请稍后重试!')
+    // 设置数据，更新渲染
+    this.setData({ userInfo })
+  },
 })
--- miniprogram/pages/profile/index.js
+++ miniprogram/pages/profile/index.js
@@ -4,7 +4,60 @@ Page({
    * 页面的初始数据
    */
   data: {},
+  getUserNickName(ev) {
+    console.log(ev.detail)
+    // 更新用户昵称
+    this.updateNickName(ev.detail.value)
+  },
+  async updateNickName(nickName) {
+    // 请求数据接口
+    const { code } = await wx.http.put('/userInfo', { nickName })
+    // 检测接口调用的结果
+    if (code !== 10000) return wx.utils.toast('更新用户信息失败!')
 
+    // 保存用户昵称
+    this.setData({ 'userInfo.nickName': nickName })
+  },
+  getUserAvatar(ev) {
+    // 用户头像地址
+    // console.log(ev.detail.avatarUrl)
+    this.updateUserAvatar(ev.detail.avatarUrl)
+  },
+
+  updateUserAvatar(avatarUrl) {
+    // 调用接口上传图片
+    wx.uploadFile({
+      url: wx.http.baseURL + '/upload',
+      filePath: avatarUrl,
+      name: 'file',
+      header: {
+        Authorization: getApp().token,
+      },
+      formData: {
+        type: 'avatar',
+      },
+      success: (res) => {
+        // 转换 json 数据
+        const data = JSON.parse(res.data)
+        // 检测接口调用结果
+        if (data.code !== 10000) return wx.utils.toast('更新头像失败!')
+
+        // 保存并预览图片地址
+        this.setData({ 'userInfo.avatar': data.data.url })
+      },
+    })
+  },
+  async getUserInfo() {
+    const { code, data } = await wx.http.get('/userInfo')
+    if (code !== 10000) return wx.utils.toast()
+    // 保存数据
+    this.setData({
+      userInfo: {
+        avatar: data.avatar,
+        nickName: data.nickName,
+      },
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
